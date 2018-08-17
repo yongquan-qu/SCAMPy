e675d3e1254cee63976317a50a720cac61a96848
Anna Jaruga <ajaruga@caltech.edu>
2018-08-17 13:15:46 -0700

    ....

--- setup.py
+++ setup.py
@@ -57,7 +57,6 @@ elif 'sampo' in platform.node():
     f_compiler = 'gfortran'
 elif 'linux' in sys.platform:
     #Compile flags for Travis (linux)
-
     library_dirs = []
     libraries = []
     #libraries.append('mpi')
@@ -66,15 +65,11 @@ elif 'linux' in sys.platform:
     extra_compile_args  = []
     extra_compile_args += ['-std=c99', '-O3', '-march=native', '-Wno-unused',
                            '-Wno-#warnings', '-Wno-maybe-uninitialized', '-Wno-cpp', '-Wno-array-bounds','-fPIC']
-
     from distutils.sysconfig import get_python_lib
     tmp_path = get_python_lib()
-    print(">>>", tmp_path)
-
     netcdf_include = tmp_path + '/netcdf4/include'
     netcdf_lib = tmp_path + "/netcdf4/lib"
     f_compiler = 'gfortran'
-
 else:
     print('Unknown system platform: ' + sys.platform  + 'or unknown system name: ' + platform.node())
     sys.exit()
