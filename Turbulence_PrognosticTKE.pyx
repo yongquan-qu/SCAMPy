ccb5ef97a4fab8a8c1b861e25a707cfdda2e9fd7
yairchn <yairchn@caltech.edu>
2019-06-30 23:39:08 -0700

    ..

--- Turbulence_PrognosticTKE.pyx
+++ Turbulence_PrognosticTKE.pyx
@@ -892,10 +892,10 @@ cdef class EDMF_PrognosticTKE(ParameterizationBase):
                         a = self.UpdVar.Area.values[i,k]
                         wu_half = interp2pt(self.UpdVar.W.values[i,k], self.UpdVar.W.values[i,k-1])
                         dw = (wu_half - we_half)
-                        #self.horizontal_KM[i,k] = a*(1.0-a)*self.tke_ed_coeff*sqrt(fmax(GMV.TKE.values[k],0.0))*l
-                        #self.horizontal_KH[i,k] = a*(1.0-a)*self.horizontal_KM[i,k] / self.prandtl_number
-                        self.horizontal_KM[i,k] = a*self.tke_ed_coeff*sqrt(fmax(a*ae[k]*dw*dw,0.0))*l
-                        self.horizontal_KH[i,k] = a*self.horizontal_KM[i,k] / self.prandtl_number
+                        self.horizontal_KM[i,k] = a*(1.0-a)*self.tke_ed_coeff*sqrt(fmax(GMV.TKE.values[k],0.0))*l
+                        self.horizontal_KH[i,k] = a*(1.0-a)*self.horizontal_KM[i,k] / self.prandtl_number
+                        #self.horizontal_KM[i,k] = a*self.tke_ed_coeff*sqrt(fmax(a*ae[k]*dw*dw,0.0))*l
+                        #self.horizontal_KH[i,k] = a*self.horizontal_KM[i,k] / self.prandtl_number
                     else:
                         self.horizontal_KM[i,k] = 0.0
                         self.horizontal_KH[i,k] = 0.0
