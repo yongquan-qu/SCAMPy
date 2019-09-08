1dd2152c9f57f13ddcf5a0ea0456674400ff8b59
Anna Jaruga <ajaruga@caltech.edu>
2019-09-07 22:04:27 -0700

    extend eos test to 18km

--- tests/unit/test_eos.py
+++ tests/unit/test_eos.py
@@ -10,6 +10,7 @@ from netCDF4 import Dataset
 
 import pprint as pp
 import numpy as np
+import math as mt
 
 import pytest
 import pytest_wrapper as wrp
@@ -18,7 +19,7 @@ import pytest_wrapper as wrp
 from hypothesis import given, strategies as st
 
 
-@given(p0   = st.floats(min_value = 30000, max_value = 101300), # pressure between 1013hPa - 300hPa
+@given(p0   = st.floats(min_value = 12000, max_value = 101300), # pressure between 1013hPa - 300hPa
        qt   = st.floats(min_value = 0,     max_value = 0.040),  # total water specific humidity between 0 - 40 g/kg
        frac = st.floats(min_value = 0,     max_value = 1))      # fraction of qt that is water vapor specific humidity
 def test_pressure(p0, qt, frac):
@@ -34,7 +35,7 @@ def test_pressure(p0, qt, frac):
     assert(np.isclose(p0, pd + pv, rtol = 1e-12))
 
 
-@given(p0 = st.floats(min_value = 30000,  max_value = 101300),      # pressure between 1013hPa - 300hPa
+@given(p0 = st.floats(min_value = 12000,  max_value = 101300),      # pressure between 1013hPa - 300hPa
        qt = st.floats(min_value = 0,      max_value = 0.040),       # total water specific humidity between 0 - 40 g/kg
        T  = st.floats(min_value = 273.15, max_value = 273.15 + 40)) # temperature between 0 - 40 C
 def test_eos_first_guess(p0, qt, T):
@@ -60,7 +61,7 @@ def test_eos_first_guess(p0, qt, T):
     assert(np.isclose(T, T_new, rtol = 1e-12))
 
 
-@given(z  = st.floats(min_value = 0,     max_value = 11000), # height in the atmosphere around which we will test
+@given(z  = st.floats(min_value = 0,     max_value = 18000), # height in the atmosphere around which we will test
        rnd= st.floats(min_value = -0.05, max_value = 0.05),  # random factor
        ql = st.floats(min_value = 0,     max_value = 0.010)) # liquid water specific humidity between 0 - 10 g/kg
 def test_eos_saturated(z, rnd, ql):
@@ -76,12 +77,25 @@ def test_eos_saturated(z, rnd, ql):
     const = wrp.scampy_constants()
 
     # "standard atmosphere" profile around which we will test
+    # https://en.wikipedia.org/wiki/International_Standard_Atmosphere
     dTdz = -0.0065
-    T0   = 273.15 + 15
+    T0 = 273.15 + 15
+    T1 = 273.15 - 56.5
+    p0 = 101300.
+    p1 = 22632
+    z1 = 11000
     def T_standard(z):
-        return T0 + dTdz * z
-    def p_standard(T):
-        return 101300. / (1 + const.g * z / T / const.Rd)
+        T = T1
+        if z < z1:
+            T = T0 + dTdz * z
+        return T
+    def p_standard(z):
+        p = p0
+        if z < z1:
+            p = p0 * mt.exp(-const.g / const.Rd / dTdz * mt.log(1. + dTdz / T0 * z))
+        else:
+            p = p1 * mt.exp(-const.g / const.Rd / T1 * (z - z1))
+        return p
 
     T    = T_standard(z) / (1. - rnd)
     p    = p_standard(T) / (1. - rnd)
