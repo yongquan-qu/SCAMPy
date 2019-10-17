efac9970c2fde653c76f511e07163694d65999ea
Anna Jaruga <ajaruga@caltech.edu>
2019-10-17 15:33:31 -0700

    make rain clima_1m default

--- generate_namelist.py
+++ generate_namelist.py
@@ -140,8 +140,8 @@ def Rico(namelist_defaults):
     namelist['time_stepping']['dt'] = 20.0
     namelist['time_stepping']['t_max'] = 86400.0
 
-    namelist['microphysics']['rain_model'] = 'cutoff'
-    #namelist['microphysics']['rain_model'] = 'clima_1m'
+    #namelist['microphysics']['rain_model'] = 'cutoff'
+    namelist['microphysics']['rain_model'] = 'clima_1m'
 
     namelist['meta']['simname'] = 'Rico'
     namelist['meta']['casename'] = 'Rico'
@@ -158,8 +158,8 @@ def TRMM_LBA(namelist_defaults):
     namelist['time_stepping']['dt'] = 30.0
     namelist['time_stepping']['t_max'] = 21600.0
 
-    namelist['microphysics']['rain_model'] = 'cutoff'
-    #namelist['microphysics']['rain_model'] = 'clima_1m'
+    #namelist['microphysics']['rain_model'] = 'cutoff'
+    namelist['microphysics']['rain_model'] = 'clima_1m'
 
     namelist['meta']['simname'] = 'TRMM_LBA'
     namelist['meta']['casename'] = 'TRMM_LBA'
