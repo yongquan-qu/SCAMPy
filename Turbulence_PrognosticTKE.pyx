eea9183b3662063f94c3f517fea258b42da1364e
yairchn <yairchn@caltech.edu>
2019-10-18 16:36:51 -0700

    fix pressure plume spacing in diagnostic updrafts

--- Turbulence_PrognosticTKE.pyx
+++ Turbulence_PrognosticTKE.pyx
@@ -623,7 +623,7 @@ cdef class EDMF_PrognosticTKE(ParameterizationBase):
             entr_struct ret
             entr_in_struct input
             double a,b,c, w, w_km,  w_mid, w_low, denom, arg
-            double entr_w, detr_w, B_k, area_k, w2
+            double entr_w, detr_w, B_k, area_k, w2, pressure_term_
 
         self.set_updraft_surface_bc(GMV, Case)
         self.compute_entrainment_detrainment(GMV, Case)
@@ -677,8 +677,12 @@ cdef class EDMF_PrognosticTKE(ParameterizationBase):
                         entr_w = interp2pt(self.entr_sc[i,k], self.entr_sc[i,k+1])
                         detr_w = interp2pt(self.detr_sc[i,k], self.detr_sc[i,k+1])
                         B_k = interp2pt(self.UpdVar.B.values[i,k], self.UpdVar.B.values[i,k+1])
+                        if self.use_const_plume_spacing:
+                            presure_term_ = 1.0/self.constant_plume_spacing
+                        else:
+                            presure_term_ = 1.0/ (self.aspect_ratio*self.UpdVar.updraft_top[i]*sqrt(fmax(area_k,self.minimum_area)))
                         w2 = ((self.vel_buoy_coeff * B_k + 0.5 * w_km * w_km * dzi)
-                              /(0.5 * dzi +entr_w + (1.0/(self.aspect_ratio*self.UpdVar.updraft_top[i]))/sqrt(fmax(area_k,self.minimum_area))))
+                              /(0.5 * dzi +entr_w + pressure_term_))
                         if w2 > 0.0:
                             self.UpdVar.W.values[i,k] = sqrt(w2)
                         else:
