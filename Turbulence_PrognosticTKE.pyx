2cbcec23c84570c63b8bca5b95aeaa76403bc1f6
Ignacio <ilopezgo@caltech.edu>
2019-11-18 09:18:38 -0800

    Modified smooth monimum formulation according to draft.

--- Turbulence_PrognosticTKE.pyx
+++ Turbulence_PrognosticTKE.pyx
@@ -954,7 +954,7 @@ cdef class EDMF_PrognosticTKE(ParameterizationBase):
                     j += 1
 
                 self.mls[k] = np.argmin(l)
-                self.mixing_length[k] = auto_smooth_minimum(l, 0.1)
+                self.mixing_length[k] = lamb_smooth_minimum(l, 0.1)
                 self.ml_ratio[k] = self.mixing_length[k]/l[int(self.mls[k])]
 
         else:
@@ -1350,7 +1350,7 @@ cdef class EDMF_PrognosticTKE(ParameterizationBase):
             if self.use_const_plume_spacing:
                 self.pressure_plume_spacing[i] = self.constant_plume_spacing
             else:
-                self.pressure_plume_spacing[i] = fmax(self.aspect_ratio*self.UpdVar.updraft_top[i], 500)
+                self.pressure_plume_spacing[i] = fmax(self.aspect_ratio*self.UpdVar.updraft_top[i], 250.0)
         return
 
     cpdef compute_nh_pressure(self):
