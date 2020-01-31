10202a384886465d568c4772e620d43be2cb3f8b
Ignacio <ilopezgo@caltech.edu>
2020-01-30 18:43:19 -0800

    Added namelist and paramlist files for Nieuwstadt case.

--- generate_paramlist.py
+++ generate_paramlist.py
@@ -65,6 +65,8 @@ def main():
 
     if case_name == 'Soares':
         paramlist = Soares(paramlist_defaults)
+    elif case_name == 'Nieuwstadt':
+        paramlist = Nieuwstadt(paramlist_defaults)
     elif case_name == 'Bomex':
         paramlist = Bomex(paramlist_defaults)
     elif case_name == 'life_cycle_Tan2018':
@@ -96,6 +98,13 @@ def Soares(paramlist_defaults):
 
     return paramlist
 
+def Nieuwstadt(paramlist_defaults):
+
+    paramlist = copy.deepcopy(paramlist_defaults)
+    paramlist['meta']['casename'] = 'Nieuwstadt'
+
+    return paramlist
+    
 def Bomex(paramlist_defaults):
 
     paramlist = copy.deepcopy(paramlist_defaults)
